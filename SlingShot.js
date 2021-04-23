fb5eb870832136cc2130e17779747cdeabfd7e93
themathboi101 <78664310+themathboi101@users.noreply.github.com>
2021-04-22 20:08:31 -0400

    Update SlingShot.js

--- SlingShot.js
+++ SlingShot.js
@@ -28,8 +28,5 @@ class Slingshot{
         line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
         }
-        attach(body) {
-            this.sling.bodyA=body
-            }
-    
+        
 }
